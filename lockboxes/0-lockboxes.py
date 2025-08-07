407fc1e146296f1d29e27dda7ecaf6d1f6ec8de6
Yousra <142456388+yousrakdc@users.noreply.github.com>
2025-08-07 09:38:24 +0200

    pep8 issues

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-''' 
+'''
 Determines if all boxes can be unlocked using an iterative approach.
 Uses a stack to explore boxes without recursion limits.
 '''
 
 def canUnlockAll(boxes):
+
     if not boxes:
         return True
 
@@ -15,10 +16,10 @@ def canUnlockAll(boxes):
 
     while to_explore:
         box_num = to_explore.pop()
-        
+
         for key in boxes[box_num]:
             if key < n and key not in unlocked:
                 unlocked.add(key)
                 to_explore.append(key)
-    
+
     return len(unlocked) == n
