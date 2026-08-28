c9e5b6720c8b43ae38864c86ea7660f2a1d250b0
Yousra <142456388+yousrakdc@users.noreply.github.com>
2026-08-28 14:26:28 +0200

    test

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,53 +1,30 @@
-#!/usr/bin/node
+#!/usr/bin/env
 
 const request = require('request');
 
 const movieId = process.argv[2];
+const BASE_URL = 'https://swapi.dev/api/films/';
 
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
+function printCharacter(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+  request(characters[index], (err, res, body) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const character = JSON.parse(body);
+    console.log(character.name);
+    printCharacter(characters, index + 1);
+  });
 }
 
-const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+request(`${BASE_URL}${movieId}/`, (err, res, body) => {
+  if (err) {
+    console.error(err);
     return;
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
-    return;
-  }
-
   const film = JSON.parse(body);
-  const characters = film.characters;
-
-  if (!characters || characters.length === 0) {
-    return;
-  }
-
-  let completed = 0;
-  const characterNames = new Array(characters.length);
-
-  characters.forEach((characterUrl, index) => {
-    request(characterUrl, (err, res, charBody) => {
-      if (err) {
-        console.error('Error:', err);
-        return;
-      }
-
-      const character = JSON.parse(charBody);
-      characterNames[index] = character.name;
-      completed++;
-
-      if (completed === characters.length) {
-        characterNames.forEach(name => {
-          console.log(name);
-        });
-      }
-    });
-  });
+  printCharacter(film.characters, 0);
 });
